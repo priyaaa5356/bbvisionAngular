5d8ebd4752473900e4237c631add78f27fe0d377
azhagu <azhagan15041996@gmail.com>
2021-08-21 12:28:30 +0530

    test azhaguraja

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -376,7 +376,7 @@ export class DashboardComponent implements OnInit {
     this.dataSourceamc = this.amc;
   }
   async getleavetype() {
-    //azsdfasdfasdfasdf
+    //azasdfasdfasdfasdf
 
 
     var sss = sessionStorage.getItem('logindet');
