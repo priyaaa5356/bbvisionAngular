885d94f597da805db6ec0d96d70387e0dc12bb12
sindhuguna <sindhu.g@bluebase.in>
2021-08-27 10:36:37 +0530

    sindhu 27-08-21 morning

---
+++ src/app/Resource/model/jobdescriptionlistmaster.ts
@@ -0,0 +1,10 @@
+export class JobdescriptionlistMaster {
+    id:string = "";
+    name: string = "";
+    name1: string = "";
+    location: string = "";
+    experience: string = "";
+    joining: string = "";
+    closing:string = "";
+    consultant:string = "";
+}
