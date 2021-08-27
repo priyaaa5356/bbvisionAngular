964a819bb16544b4627b6a63ca8f1519cf917b8b
priyaaa5356 <priyaaa5356@gmail.com>
2021-08-27 10:27:59 +0530

    preethi

--- src/app/Resource/model/resourcemodel.ts
+++ src/app/Resource/model/resourcemodel.ts
@@ -1,70 +1,70 @@
 
 export class Resource {
-   source:string="";
-   referelname:string="";
-   date:string="";
-   consultant:string="";
-   postappliedfor:string="";
-   firstname:string="";
-   referaltype:string="";
-   consultantadd:number=0;
-   lastname:string="";
-   gender:string="";
-   referaladdshow:string="";
-   referalname:string="";
-   mobilenumber:string="";
-   whatsappnumber: string="";
-   emailid:string="";
-   adhaarnumber:string="";
-   degree:string="";
-   yearsofpassout:string="";
-   percentage:string="";
-   employeestatus:string="";
-   companyname: string="";
-   totalexperience: string="";
-   noofyearexperience: string="";
-   college:string="";
-   emloyeestatus:string="";
-   releventexperience: string="";
-   expectedctc:string="";
-   currentctc: string ="";
-   certification:string="";
-   certificate:string="";
-   certifiedfrom: string="";
-   yearsofexperience:string="";
-   validity:string="";
-   resume:string="";
-   certficate:string="";
-   status:boolean=false;
+    source: string = "";
+    referelname: string = "";
+    date: string = "";
+    consultant: string = "";
+    postappliedfor: string = "";
+    firstname: string = "";
+    referaltype: string = "";
+    consultantadd: number = 0;
+    lastname: string = "";
+    gender: string = "";
+    referaladdshow: string = "";
+    referalname: string = "";
+    mobilenumber: string = "";
+    whatsappnumber: string = "";
+    emailid: string = "";
+    adhaarnumber: string = "";
+    degree: string = "";
+    yearsofpassout: string = "";
+    percentage: string = "";
+    employeestatus: string = "";
+    companyname: string = "";
+    totalexperience: string = "";
+    noofyearexperience: string = "";
+    college: string = "";
+    emloyeestatus: string = "";
+    releventexperience: string = "";
+    expectedctc: string = "";
+    currentctc: string = "";
+    certification: string = "";
+    certificate: string = "";
+    certifiedfrom: string = "";
+    yearsofexperience: string = "";
+    validity: string = "";
+    resume: string = "";
+    certficate: string = "";
+    status: boolean = false;
 
 
 }
 
-export class sourcetypeselect {
+export class Sourcetypeselect {
     sourcetype: string = "";
     sourcetypecode: number = 0;
 }
-export class consultantselect {
+export class Consultantselect {
     consultantype: string = "";
     consultantcode: number = 0;
 }
-export class referaltypeselect {
+export class Referaltypeselect {
     referaltypeadd: string = "";
     referaltypeaddcode: number = 0;
 }
-export class referalselect {
+export class Referalselect {
     referaltype: string = "";
     referaltypecode: number = 0;
 }
-export class postappliedforselect {
+export class Postappliedforselect {
     postappliedfortype: string = "";
     postappliedfortypecode: number = 0;
 }
-export class employeementstatusselect {
+export class Employeementstatusselect {
     employeementstatustype: string = "";
     employeementstatustypecode: number = 0;
 }
-export class certificationselect {
+export class Certificationselect {
     certificationtype: string = "";
     certificationtypecode: number = 0;
 }
